e535e7aa1a3d2c20597940436ba838e65da5b643
Kayo Hikawa <kayohikawa@outlook.com>
2025-09-04 09:24:53 +0800

    Added PUSHA/POPA for interrupt handler

--- src/libkernel/intr_handler.s
+++ src/libkernel/intr_handler.s
@@ -11,32 +11,41 @@
 
 page_fault_handler:
     cli
-    pushl %eax
-    movl  4(%esp), %eax
+    pushl %ebp
+    movl  %esp, %ebp
+    pushal
+    movl  4(%ebp), %eax
     pushl %eax
     movl  %cr2, %eax
     pushl %eax
     call  page_fault
     addl  $8, %esp
-    popl  %eax
-    addl  $4, %esp
+    popal
+    popl  %ebp
+    addl  $4, %esp      # Clear error code
     sti
     iret
 
 irq_0x20_handler:
     cli
+    pushal
     call  irq_0x20
+    popal
     sti
     iret
 
 irq_0x21_handler:
     cli
+    pushal
     call  irq_0x21
+    popal
     sti
     iret
 
 irq_0x27_handler:
     cli
+    pushal
     call  irq_0x27
+    popal
     sti
     iret
