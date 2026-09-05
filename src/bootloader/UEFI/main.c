f3464976cf584783fa5de083532546ecd3f91247
Kayo Hikawa <kayohikawa@outlook.com>
2026-09-05 21:22:33 +0800

    Use CMake for UEFI bootloader

--- src/bootloader/UEFI/main.c
+++ src/bootloader/UEFI/main.c
@@ -1,6 +1,6 @@
 #include <efi/efi.h>
 
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+EFI_STATUS EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello UEFI\r\n");
     while (1);
